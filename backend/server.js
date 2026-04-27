c2d7ccedda38cfd163bf113c7e7c4fb219500283
Harsh sharma <harshsharma6349@gmail.com>
2026-04-27 11:50:25 +0530

    Fix server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: process.env.CORS_ORIGIN, methods: ["GET","POST","PUT","DELETE"], credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
@@ -21,12 +27,14 @@ app.use("/api/auth", authRoutes);
 
 // MongoDB connection
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+    console.log("✅ MongoDB connected");
   })
-  .catch((err) => console.log("DB Connection Error:", err));
-
-
-  
+  .catch((err) => {
+    console.log("DB Connection Error:", err);
+  });
